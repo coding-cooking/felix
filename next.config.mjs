e4821874ea6ef72bc86c0eed6872b693514de777
fei <fei.coding2023@gmail.com>
2024-04-02 07:20:25 +1100

    deploy app on gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.pexels.com",
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+  output: "export",
+  distDir: "docs",
 };
 
 export default nextConfig;
