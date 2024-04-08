fd5c2e3bb7df33dae9a86373f212eb4642b8ceec
coding-cooking <147131996+coding-cooking@users.noreply.github.com>
2024-04-08 22:44:01 +1000

    add google analytics code (#9)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import HeaderBar from "@/components/HeaderBar";
 import Footer from "@/components/Footer";
 import { useEffect, useState } from "react";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,21 @@ export default function RootLayout({
   }, []);
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics tracking code */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZH9RXZMLJM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-ZH9RXZMLJM');
+      `,
+          }}
+        />
+      </Head>
+
       <body className={inter.className}>
         <AppRouterCacheProvider options={{ key: "fei" }}>
           <HeaderBar />
