f05ff530ee772b73c3bb8351c6c8c3d91f291b1f
Coding-Cooking <fei.coding2023@gmail.com>
2024-04-08 18:38:13 +1000

    address review

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { fetchArticles } from "./lib/actions";
 import Subscription from "@/components/Subscription";
 
 export const metadata: Metadata = {
-  title: "Felix's Website",
-  description: "A record of a new immigrant settled in Sydney.",
+  title: "Fei Zhang | Felix | Software | Blog | Web Development | felix-one.vercel.app",
+  description: "My name is Felix, I'm a software engineer based in Sydney, I'd love to provide high standard services regarding web development on both the frontend and backend. My email can be accessed on my blog website: felix-one.vercel.app.",
 };
 
 type HomepageProps = {
