1d7497d6ef6c7e6bfc038db9bc69b1f0491b69de
coding-cooking <147131996+coding-cooking@users.noreply.github.com>
2024-04-08 18:38:55 +1000

    optimize the metadata and favicon (#7)
    * optimize the metadata and favicon
    
    * update the seo info
    
    * address review

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from "next";
 import CardList from "@/components/CardList";
 import { fetchArticles } from "./lib/actions";
 import Subscription from "@/components/Subscription";
+
+export const metadata: Metadata = {
+  title: "Fei Zhang | Felix | Software | Blog | Web Development | felix-one.vercel.app",
+  description: "My name is Felix, I'm a software engineer based in Sydney, I'd love to provide high standard services regarding web development on both the frontend and backend. My email can be accessed on my blog website: felix-one.vercel.app.",
+};
 
 type HomepageProps = {
   searchParams: {
@@ -9,13 +15,13 @@ type HomepageProps = {
 }
 
 export default async function Homepage({ searchParams }: HomepageProps) {
-  
+
   const q = searchParams?.q;
   const articles = await fetchArticles();
-  
+
   return (
     <>
-      <CardList articles={articles} q={q}/>
+      <CardList articles={articles} q={q} />
       <Subscription />
     </>
   );
