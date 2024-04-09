7abe67ed950757861b11d79686f4ad090b417c87
Coding-Cooking <fei.coding2023@gmail.com>
2024-04-09 15:00:30 +1000

    update gtm code

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,21 +24,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        {/* Google Analytics tracking code */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZH9RXZMLJM"></script>
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-ZH9RXZMLJM');
-      `,
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TTVSHND3');
+            `,
           }}
         />
+        {/* End Google Tag Manager */}
       </Head>
-
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TTVSHND3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <AppRouterCacheProvider options={{ key: "fei" }}>
           <HeaderBar />
           {windowHeight && (
