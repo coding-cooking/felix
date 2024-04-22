9b52ee6ba522ce987b87fe325a69cd41527b6280
coding-cooking <147131996+coding-cooking@users.noreply.github.com>
2024-04-22 21:55:32 +1000

    Share (#15)
    * build the share feature
    
    * build the share feature
    
    * update share feature

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -6,9 +6,10 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PaginationCard from "./PagenationCard";
 import { CardImage } from "./CardImage";
+import { ShareButtons } from "./ShareButtons";
 
 export type ArticleInterface = {
   title: string,
@@ -26,13 +27,15 @@ type CardListProps = {
 export default function CardList({ articles, q }: CardListProps) {
   const [searchedArticles, setSearchedArticles] = useState<ArticleInterface[]>(articles);
   const [page, setPage] = useState<number>(1);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [showShareButtons, setShowShareButtons] = useState(false);
+  const shareButtonsTimeout = useRef<NodeJS.Timeout | null > (null);
 
   function paginate(articles: ArticleInterface[], page: number, pageSize: number) {
     const startIndex = (page - 1) * pageSize;
     const endIndex = startIndex + pageSize - 1;
     const sortedArticles = articles.sort((a: ArticleInterface, b: ArticleInterface) => Number(b.parsedName) - Number(a.parsedName));
     return sortedArticles.slice(startIndex, endIndex + 1);
-     
   }
 
   useEffect(() => {
@@ -44,6 +47,22 @@ export default function CardList({ articles, q }: CardListProps) {
       : articles;
     setSearchedArticles(_articles);
   }, [q, articles])
+  
+
+  const handleMouseEnter = (index: number) => {
+    if (shareButtonsTimeout.current){
+      clearTimeout(shareButtonsTimeout.current);
+    }
+    setHoveredIndex(index);
+    setShowShareButtons(true);
+  };
+
+  const handleMouseLeave = () => {
+    shareButtonsTimeout.current = setTimeout(() => {
+      setHoveredIndex(-1);
+      setShowShareButtons(false);
+    }, 1000); 
+  };
 
   return (
     <Container maxWidth="xl">
@@ -57,31 +76,36 @@ export default function CardList({ articles, q }: CardListProps) {
           marginBottom: "30px",
         }}
       >
-        {paginate(searchedArticles, page, 9)?.map((article) => {
-            return (
-              <Card key={article.title} sx={{ width: 345 }}>
-                <CardActionArea>
-                  <CardImage article={article} />
-                  <CardContent>
-                    <Typography variant="h5" component="div" gutterBottom>
-                      {article.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" height={80}>
-                      {`${article.content.slice(0, 70)}...`}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    Share
-                  </Button>
-                  <Link href={`/${article.parsedName}`}>
-                    <Button size="small">Learn More</Button>
-                  </Link>
-                </CardActions>
-              </Card>
-            );
-          })}
+        {paginate(searchedArticles, page, 9)?.map((article,index) => {
+          return (
+            <Card key={article.title} sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardImage article={article}/>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {article.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" height={80}>
+                    {`${article.content.slice(0, 70)}...`}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions sx={{ position: "relative", paddingTop: "10px" }}>
+                <Button
+                  size="small"
+                  color="primary"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}>
+                  Share
+                </Button>
+                {showShareButtons && hoveredIndex === index && <ShareButtons article={article} setShowShareButtons={setShowShareButtons} index={index} setHoveredIndex={setHoveredIndex} />}
+                <Link href={`/${article.parsedName}`}>
+                  <Button size="small">Learn More</Button>
+                </Link>
+              </CardActions>
+            </Card>
+          );
+        })}
       </Box>
       <PaginationCard searchedArticles={searchedArticles} page={page} setPage={setPage} />
     </Container>
