638652a230f21da908919b887b8605fbbc4369e4
Coding-Cooking <fei.coding2023@gmail.com>
2024-04-27 22:42:51 +1000

    update share card

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { GoogleTagManager } from '@next/third-parties/google';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { usePathname } from 'next/navigation';
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,19 @@ export default function RootLayout({
     <html lang="en">
       <GoogleTagManager gtmId="GTM-TTVSHND3" />
       <GoogleAnalytics gaId="G-ZH9RXZMLJM" />
+      <Head>
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="https://images.pexels.com/photos/21300075/pexels-photo-21300075/free-photo-of-sydney-sea.jpeg" />
+        <meta property="og:title" content="Felix's website" />
+        <meta property="og:description" content="a website about life, reading and technology" />
+        <meta property="og:url" content={`https://felix-one.vercel.app`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@felixzhang" />
+        <meta name="twitter:title" content="Felix's website" />
+        <meta name="twitter:description" content="a website about life, reading and technology" />
+        <meta name="twitter:image" content="https://images.pexels.com/photos/21300075/pexels-photo-21300075/free-photo-of-sydney-sea.jpeg" />
+        <meta name="twitter:url" content={`https://felix-one.vercel.app`} />
+      </Head>
 
       <body className={inter.className}>
         <AppRouterCacheProvider options={{ key: "fei" }}>
