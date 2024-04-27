32bbe8e6523730c442119dd2cafb0bbd1fe2465b
Coding-Cooking <fei.coding2023@gmail.com>
2024-04-28 09:53:55 +1000

    update share card

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -19,7 +19,6 @@ export async function generateMetadata({ params }: Props, parent: ResolvingMetad
 
   // optionally access and extend (rather than replace) parent metadata
   // const previousImages = (await parent).openGraph?.images || []
-
   return {
     title: article.title,
     openGraph: {
