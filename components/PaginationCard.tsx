d470c2bdab166975380da6ccd0ddad9bd45fa090
Coding-Cooking <fei.coding2023@gmail.com>
2024-05-05 21:58:24 +1000

    add the mobile load more feature

--- components/PaginationCard.tsx
+++ components/PaginationCard.tsx
@@ -1,6 +1,5 @@
 import Box from "@mui/material/Box";
 import Pagination from '@mui/material/Pagination';
-import { Dispatch, SetStateAction, useEffect } from "react";
 import { ArticleInterface } from "./CardList";
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
@@ -9,24 +8,27 @@ const NUM_PER_PAGE = 9;
 type PaginationProps = {
     searchedArticles: ArticleInterface[],
     page: string,
-    // setPage: Dispatch<SetStateAction<number>>,
 }
 
 export default function PaginationCard({ searchedArticles, page }: PaginationProps) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
-    const { replace } = useRouter();
+    const { replace, push } = useRouter();
 
     const params = new URLSearchParams(searchParams);
 
     const count = (searchedArticles.length) % NUM_PER_PAGE === 0 ? (searchedArticles.length) / NUM_PER_PAGE : parseInt((searchedArticles.length / NUM_PER_PAGE).toString()) + 1;
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-        // setPage(value);
-        params.set("page", value.toString())
-        replace(`${pathname}?${params}`)
+        if (value === 1) {
+            params.delete("page");
+            push(`${pathname}?${params.toString()}`, undefined);
+        } else {
+            params.set("page", value.toString())
+            replace(`${pathname}?${params}`)
+        }
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
-    
+
     return <Box
         sx={{
             my: 6,
