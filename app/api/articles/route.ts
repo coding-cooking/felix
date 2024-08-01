d692b91d45032946cf13562c86f04b908606a661
Coding-Cooking <fei.coding2023@gmail.com>
2024-08-01 20:04:58 +1000

    test

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import Article from "@/model/Article";
 
-export async function GET() {
-    const articles = await Article.find();
-    if (!articles) return NextResponse.json("No articles found!", { status: 204 });
+export async function GET(request: Request) {
+    // const articles = await Article.find();
+    // if (!articles) return NextResponse.json("No articles found!", { status: 204 });
     return NextResponse.json([], { status: 200 });
 }
