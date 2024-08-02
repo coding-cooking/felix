c70aec146bca6679b5aee3c5ad4a103b0974657c
Coding-Cooking <fei.coding2023@gmail.com>
2024-08-02 20:38:31 +1000

    adjust

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		await mongoose.connect(process.env.DATABASE_URI);
+		await mongoose.connect(process.env.DATABASE_URI, {
+			bufferCommands: false,
+			serverSelectionTimeoutMS: 10000, // Set a higher value here
+		});
 	} catch (err) {
 		console.error(err);
 	}
