2f91830adbd59f888e0371231de246df6bfa3b99
Coding-Cooking <fei.coding2023@gmail.com>
2024-08-02 20:21:06 +1000

    format

--- app/api/articles/[id]/route.ts
+++ app/api/articles/[id]/route.ts
@@ -2,12 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import Article from "@/model/Article";
 
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-    if (!params?.id)
+    if (!params?.id) {
         return NextResponse.json({ message: 'ID parameter is required!' }, { status: 404 });
+    }
     const article = await Article.findOne({ _id: params.id }).exec();
-    console.log("article is ", article)
-    if (!article)
+    if (!article) {
         return NextResponse.json({ message: 'Not found' }, { status: 404 });
+    }
     return NextResponse.json(article, { status: 201 });
-
 }
