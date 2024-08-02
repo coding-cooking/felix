6d7c0dff047949fd371e28b2b47bf61cf1043020
Coding-Cooking <fei.coding2023@gmail.com>
2024-08-02 20:43:37 +1000

    try claude's way

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,38 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.DATABASE_URI, {
-			bufferCommands: false,
-			serverSelectionTimeoutMS: 10000, // Set a higher value here
-		});
-	} catch (err) {
-		console.error(err);
+const MONGODB_URI = process.env.DATABASE_URI;
+
+if (!MONGODB_URI) {
+	throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+	if (cached.conn) {
+		return cached.conn;
 	}
-};
+
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			socketTimeoutMS: 30000, // Increase timeout to 30 seconds
+			connectTimeoutMS: 30000, // Increase connection timeout to 30 seconds
+		};
+
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+			return mongoose;
+		});
+	}
+
+	cached.conn = await cached.promise;
+	return cached.conn;
+}
+
+export default dbConnect;
