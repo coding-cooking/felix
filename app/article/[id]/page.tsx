f5b8d0dd06811ba6154418233f5784db25114977
Coding-Cooking <fei.coding2023@gmail.com>
2024-08-10 21:58:50 +1000

    delete commento

--- app/article/[id]/page.tsx
+++ app/article/[id]/page.tsx
@@ -109,10 +109,6 @@ export default async function Article({ params }: { params: { id: string } }) {
               </Typography>
               <ArticleContent article={article} />
             </Box>
-            <Box>
-              <script defer src="https://cdn.commento.io/js/commento.js"></script>
-              <div id="commento"></div>
-            </Box>
           </Box>
           <Box sx={{ flex: 1, "@media (max-width: 768px)": { display: "none" } }}>
             <RecentArticles id={id} />
