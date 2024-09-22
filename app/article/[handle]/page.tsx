a0ec785e65b99b9fbcfde0867b92ecda899128b5
Coding-Cooking <fei.coding2023@gmail.com>
2024-09-22 13:59:27 +1000

    resolve some issues

--- app/article/[handle]/page.tsx
+++ app/article/[handle]/page.tsx
@@ -53,10 +53,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export async function generateStaticParams() {
   const articles: ArticleInterface[] = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/articles`).then((res) => res.json())
-  console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/api/articles`)
   return articles.map((article) => ({
     handle: article.handle,
   }))
+
 }
 
 export default async function Article({ params }: { params: { handle: string } }) {
