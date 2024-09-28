59385c4b1def50f8325fea84fae0bec2e107fef2
Coding-Cooking <fei.coding2023@gmail.com>
2024-09-28 19:26:13 +1000

    add disqus comments feature

--- app/article/[handle]/page.tsx
+++ app/article/[handle]/page.tsx
@@ -1,15 +1,7 @@
-import Stack from '@mui/material/Stack';
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import { notFound } from "next/navigation";
-import { ArticleImage } from "@/components/ArticleImage";
 import { ArticleInterface } from "../../../context/ArticleContext";
 import { Metadata } from 'next';
-import { RecentArticles } from '@/components/RecentArticles';
-import { ArticleContent } from '@/components/ArticleContent';
-import { ArticleTitle } from '@/components/ArticleTitle';
-import { ArticleTag } from '@/components/ArticleTag';
+import { Article } from '@/components/Article';
 
 type Props = {
   params: { handle: string }
@@ -66,7 +58,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 //   }))
 // }
 
-export default async function Article({ params }: { params: { handle: string } }) {
+export default async function ArticlePage(this: any, { params }: { params: { handle: string } }) {
 
   const { handle } = params;
 
@@ -81,44 +73,8 @@ export default async function Article({ params }: { params: { handle: string } }
 
     if (!article) return notFound();
 
-    const articleDate = new Date(article.publishedDate);
-
     return (
-      <Stack>
-        <ArticleImage article={article} />
-        <Container maxWidth="lg" sx={{
-          width: "100%",
-          display: "flex",
-          gap: "60px",
-        }}>
-          <Box
-            sx={{
-              mb: 4,
-              flexGrow: 1,
-              display: "flex",
-              flexDirection: "column",
-              flexWrap: "wrap",
-              flex: 3,
-            }}
-          >
-            <Typography variant="h4" lineHeight={2} gutterBottom sx={{ "@media (max-width: 768px)": { fontSize: "24px", fontWeight: "400" } }}>
-              <ArticleTitle article={article} />
-            </Typography>
-            <Typography variant="subtitle1" lineHeight={3} gutterBottom sx={{ color: "rgba(106, 101, 104, 1)", "@media (max-width: 768px)": { fontSize: "14px" } }}>
-              {articleDate.toLocaleDateString()}
-            </Typography>
-            <Box>
-              <Typography variant="body1" lineHeight={2} gutterBottom>
-                <ArticleTag article={article} />
-              </Typography>
-              <ArticleContent article={article} />
-            </Box>
-          </Box>
-          <Box sx={{ flex: 1, "@media (max-width: 768px)": { display: "none" } }}>
-            <RecentArticles handle={handle} />
-          </Box>
-        </Container>
-      </Stack>
+      <Article article={article} handle={handle} />
     );
   } catch (err) {
     console.error("Error fetching article:", err);
