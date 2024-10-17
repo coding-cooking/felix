26261150bf6c3af0c15102fb32ad25b7c89c20b5
Coding-Cooking <fei.coding2023@gmail.com>
2024-10-18 10:03:54 +1100

    update the susscription style

--- components/SubscriptionForm.tsx
+++ components/SubscriptionForm.tsx
@@ -30,11 +30,17 @@ const StyledInputBase = styled(InputBase)`
     `
 
 const Congratulation = styled.div`
+    background-color: rgba(7, 135, 24, 1);
+    border: none;
+    border-radius: 10px;
+    width: 100%;
     height: 100%;
     display: flex;
     gap: 20px;
     justify-content: center;
     align-items: center;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    transition: background-color 0.3s ease-in-out, transform 0.3s ease-in-out;
 `
 
 export default function SubscriptionForm() {
