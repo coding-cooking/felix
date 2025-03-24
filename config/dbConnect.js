2b92ae60d3ab3f28ce2658bf2c2013e52a0c3c1a
Coding-Cooking <fei.coding2023@gmail.com>
2025-03-24 12:20:32 +1100

    test db connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.DATABASE_URI;
 
+console.log('MONGODB_URI is',MONGODB_URI);
+
 if (!MONGODB_URI) {
     throw new Error("Please define the DATABASE_URI environment variable");
 }
 
-let cached = global.mongoose;
+// let cached = global.mongoose;
 
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
+// if (!cached) {
+//     cached = global.mongoose = { conn: null, promise: null };
+// }
 
 async function dbConnect() {
     if (cached.conn) {
