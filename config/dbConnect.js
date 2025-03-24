a3266273793b68effbf4c6e1445005eb45f0ca90
Coding-Cooking <fei.coding2023@gmail.com>
2025-03-24 12:31:39 +1100

    test connect

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,17 +2,15 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.DATABASE_URI;
 
-console.log('MONGODB_URI is',MONGODB_URI);
-
 if (!MONGODB_URI) {
     throw new Error("Please define the DATABASE_URI environment variable");
 }
 
-// let cached = global.mongoose;
+let cached = global.mongoose;
 
-// if (!cached) {
-//     cached = global.mongoose = { conn: null, promise: null };
-// }
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
 
 async function dbConnect() {
     if (cached.conn) {
@@ -29,6 +27,8 @@ async function dbConnect() {
         };
 
         try {
+            console.log("MONGODB_URI is", MONGODB_URI, "trying to connect");
+
             cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
                 console.log('✅ MongoDB connected successfully');
                 return mongoose;
@@ -43,7 +43,7 @@ async function dbConnect() {
         cached.conn = await cached.promise;
     } catch (error) {
         console.error('❌ Error awaiting MongoDB connection:', error);
-        cached.promise = null; // Reset the promise so we can try again
+        cached.promise = null; 
         throw error;
     }
 
